5a168944e7a08425132ba92c9d3d918074afe062
rq-J <hyungcristopher@gmail.com>
2023-04-13 14:45:55 +0800

    formula to table

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # feedmill_system
 a feedmill system for specific company
+paired with dashboard(another projecr, security)
+
+instructions:
+composer update/install
+database/seeders/userseeder
+php artisan migrate
+php artisan serve
+
+delete composer.lock
+composer update
+cp .env.example .env
--- resources/views/livewire/add-formula.blade.php
+++ resources/views/livewire/add-formula.blade.php
@@ -4,7 +4,7 @@
             <label for="myMacroSelect" class="col-form-label">Macro</label>
             <select name="" id="myMacroSelect" class="form-control">
                 @foreach ($arrMacro as $macro)
-                <option value="{{ $macro->raw_material_name }}">{{ $macro->raw_material_name }}</option>
+                <option value="{{ $macro->id }}">{{ $macro->raw_material_name }}</option>
                 @endforeach
             </select>
 
@@ -13,7 +13,7 @@
             <label for="myMicroSelect" class="col-form-label">Micro</label>
             <select name="" id="myMicroSelect" class="form-control">
                 @foreach ($arrMicro as $micro)
-                <option value="{{ $micro->raw_material_name }}">{{ $micro->raw_material_name }}</option>
+                <option value="{{ $micro->id }}">{{ $micro->raw_material_name }}</option>
                 @endforeach
             </select>
 
@@ -22,21 +22,33 @@
             <label for="myMedicineSelect" class="col-form-label">Medicine</label>
             <select name="" id="myMedicineSelect" class="form-control">
                 @foreach ($arrMedicine as $medicine)
-                <option value="{{ $medicine->raw_material_name }}">{{ $medicine->raw_material_name }}</option>
+                <option value="{{ $medicine->id }}">{{ $medicine->raw_material_name }}</option>
                 @endforeach
             </select>
         </div>
         <div class="col-6">
             <div class="container">
                 <h5>Ingredients</h5>
-                <label for="myMacroList" class="col-form-label">Macro</label>
+                {{-- <label for="myMacroList" class="col-form-label">Macro</label>
                 <ul id="myMacroList"></ul>
                 <label for="myMicroList" class="col-form-label">Micro</label>
                 <ul id="myMicroList"></ul>
                 <label for="myMedicineList" class="col-form-label">Medicine</label>
-                <ul id="myMedicineList"></ul>
+                <ul id="myMedicineList"></ul> --}}
+
+                <table id="myTable" class="table table-hover table-bordered text-center">
+                    <thead>
+                      <tr>
+                        <th>Option Name</th>
+                        <th>Input Textbox</th>
+                      </tr>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
                 <button id="addButton" class="btn btn-primary" style="float:right;">Add</button>
                 <button id="undoButton" class="btn btn-danger" style="float:right;">Reset</button>
+
+
             </div>
         </div>
     </div>
@@ -49,32 +61,51 @@
     $('#myMacroSelect').val('');
     $('#myMicroSelect').val('');
     $('#myMedicineSelect').val('');
+    var selectedOptions = []; // Array to store selected options
 
     // Hide the selected option from the select element
     $('#myMacroSelect').on('change', function() {
       var selectedOption = $(this).val();
+      var selectedOptionName = $(this).find('option[value="' + selectedOption + '"]').text();
       $(this).find('option[value="' + selectedOption + '"]').hide();
 
       // Show the selected option in the ul list
-      $('<li>').text(selectedOption).appendTo('#myMacroList');
+      $('<li>').text(selectedOptionName).appendTo('#myMacroList');
+
+      // Add selected option to the array
+      selectedOptions.push(selectedOption);
+
+      // Add selected option to the table with input textbox
+      var $row = $('<tr>');
+      $('<td>').text(selectedOptionName).appendTo($row);
+      $('<td>').append($('<input class="form-control">').attr('type', 'number').appendTo($row)).appendTo($row);
+      $row.appendTo('#myTable tbody');
     });
 
     // Hide the selected option from the select element
     $('#myMicroSelect').on('change', function() {
       var selectedOption = $(this).val();
+      var selectedOptionName = $(this).find('option[value="' + selectedOption + '"]').text();
       $(this).find('option[value="' + selectedOption + '"]').hide();
 
       // Show the selected option in the ul list
-      $('<li>').text(selectedOption).appendTo('#myMicroList');
+      $('<li>').text(selectedOptionName).appendTo('#myMicroList');
+
+      // Add selected option to the array
+      selectedOptions.push(selectedOption);
     });
 
     // Hide the selected option from the select element
     $('#myMedicineSelect').on('change', function() {
       var selectedOption = $(this).val();
+      var selectedOptionName = $(this).find('option[value="' + selectedOption + '"]').text();
       $(this).find('option[value="' + selectedOption + '"]').hide();
 
       // Show the selected option in the ul list
-      $('<li>').text(selectedOption).appendTo('#myMedicineList');
+      $('<li>').text(selectedOptionName).appendTo('#myMedicineList');
+
+      // Add selected option to the array
+      selectedOptions.push(selectedOption);
     });
 
     // Handle undo button click
@@ -85,15 +116,21 @@
         $('#myMicroSelect').find('option[value="' + selectedOption + '"]').show();
         $('#myMedSelect').find('option[value="' + selectedOption + '"]').show();
 
-
         // Remove the corresponding li element from the ul list
         $('#myList li').filter(function() {
           return $(this).text() === selectedOption;
         }).remove();
 
         selectedOption = null;
+        selectedOptionName = null;
+        selectedOptions = null;
       }
     });
+
+    // Handle collect button click
+      $('#addButton').on('click', function() {
+        // console.log(selectedOptions); // Array of selected option names
+      });
   });
   </script>
 @endsection
