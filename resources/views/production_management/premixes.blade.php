7a303c4e33c26ca41b19274a864cee49fb9897be
rq-J <hyungcristopher@gmail.com>
2023-05-10 19:18:22 +0800

    title

--- resources/views/production_management/premixes.blade.php
+++ resources/views/production_management/premixes.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-	Dashboard
+	Premixes
 @endsection
 
 @section('content')
--- resources/views/production_management/production_order.blade.php
+++ resources/views/production_management/production_order.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Dashboard
+    Production Order
 @endsection
 
 @section('content')
